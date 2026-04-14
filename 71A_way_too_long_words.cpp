8986a8c507c086abd9d4f5ff9fc5f32c7a7f6483
KunwarPrabhat <rajkunwarprabhat@gmail.com>
2026-04-14 09:36:13 +0530

    update

--- 71A_way_too_long_words.cpp
+++ 71A_way_too_long_words.cpp
@@ -1,4 +1,37 @@
-#include<iostream>
-int main (){
-    
+#include <iostream>
+#include <string>
+
+void abbreviation(std::string arr)
+{
+    int size = arr.length();
+    std::string result = "";
+
+    result += arr[0];
+    result += std::to_string(size - 2);
+    result += arr[size - 1];
+
+    std::cout << result << std::endl;
+}
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    while (n--)
+    {
+        std::string arr;
+        std::cin >> arr;
+
+        if (arr.length() > 10)
+        {
+            abbreviation(arr);
+        }
+        else
+        {
+            for (int i = 0; i < arr.length(); i++)
+            {
+                std::cout << arr[i];
+            }std::cout<<std::endl;
+        }
+    }
 }
